dc01d83fb66d127fdfaa6af948e6133c64472037
Olly Smith <olly.smith@gmail.com>
2011-07-14 17:43:17 +0100

    First working version with tests.

---
+++ fastspring.py
@@ -0,0 +1,151 @@
+# Copyright (C) 2011 by OD Consultancy Ltd.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""Fastspring/Saasy client library.
+
+"""
+
+import re
+import urllib
+from xml.etree import ElementTree
+import httplib2
+
+class FastSpringRequestError(Exception):
+    
+    def __init__(self, response, content):
+        self.code = response.status
+        self.content = content
+        Exception.__init__(self, u"%d %s" % (response.status, content))
+
+
+class XMLResponse(object):
+    """A really simple object for querying XML data returned from 
+    fastspring requests.
+    
+    """
+    def __init__(self, xml):
+        self.xml = ElementTree.XML(xml)
+    
+    def __getitem__(self, key):
+        attr = self.xml.find(key)
+        return attr != None and attr.text or None    
+
+
+class Subscription(XMLResponse):
+    """A response from the get_subscription call.
+    
+    """
+    def __init__(self, xml):
+        super(Subscription, self).__init__(xml)
+    
+    @property
+    def is_active(self):
+        return self['status'] == 'active'
+
+
+class FastSpring(object):
+    """FastSpring interface class."""
+    
+    def __init__(self, company, username, password):
+        """Create a new instance, set up auth parameters.
+        
+        """
+        self.api_url_base = "https://api.fastspring.com/company/"+company
+        self.store_url_base = "://sites.fastspring.com/"+company
+        self.company = company
+        self.username = username
+        self.password = password
+        self.auth_params = urllib.urlencode({"user": username, "pass": password})
+        self.http = httplib2.Http()
+    
+    def get_subscription(self, reference):
+        """Get subscription details."""
+        url = "%s/subscription/%s?%s" % (self.api_url_base, reference, self.auth_params)
+        response, content = self.http.request(url)
+        if response.status != 200:
+            raise FastSpringRequestError(response, content)
+        return Subscription(content)
+    
+    def update_subscription(self, reference, product_path=None, quantity=None, no_end_date=None, coupon=None, proration=None):
+        """Update subscription details.
+        
+        <subscription>
+            <productPath>(optional)</productPath>
+            <quantity>(optional)</quantity>
+            <no-end-date>(optional)</no-end-date>
+            <coupon>(optional)</coupon>
+            <proration>true|false (optional)</proration>
+        </subscription>
+        
+        """
+        root = ElementTree.Element("subscription")
+        if product_path is not None:
+            ElementTree.SubElement(root, "productPath").text = product_path
+        if quantity is not None:
+            ElementTree.SubElement(root, "quantity").text = unicode(quantity)
+        if no_end_date:
+            ElementTree.SubElement(root, "no-end-date")
+        if coupon is not None:
+            ElementTree.SubElement(root, "coupon").text = coupon
+        if proration is not None:
+            ElementTree.SubElement(root, "proration").text = proration and "true" or "false"
+        url = "%s/subscription/%s?%s" % (self.api_url_base, reference, self.auth_params)
+        response, content = self.http.request(url, method="PUT", body=ElementTree.tostring(root))
+        if response.status != 200:
+            raise FastSpringRequestError(response, content)
+
+    def delete_subscription(self, reference):
+        """Delete a subscription.
+        
+        """
+        url = "%s/subscription/%s?%s" % (self.api_url_base, reference, self.auth_params)
+        response, content = self.http.request(url, method="DELETE")
+        if response.status != 200:
+            raise FastSpringRequestError(response, content)
+        return Subscription(content)
+    
+    def get_localised_price(self, product_path, remote_addr, x_forwarded_for, accept_language):
+        """Get a localisted price based on HTTP request headers.
+        
+        Returns a HTML-ified value (eg $10.00 or 10.00&eur;).
+        
+        """
+        args = urllib.urlencode({"product_1_path": product_path,
+                                 "user_remote_addr": remote_addr,
+                                 "user_x_forwarded_for": x_forwarded_for,
+                                 "user_accept_language": accept_language})
+        url = "http%s/api/price?%s" % (self.store_url_base, args)
+        response, content = self.http.request(url)
+        if response.status != 200:
+            raise FastSpringRequestError(response, content)
+        m = re.search(r"^product_1_unit_html=(.+)$", content, re.M)
+        if m:
+            return m.group(1)
+        else:
+            return None
+
+    def short_order_url(self, product_path, referrer=None):
+        """Get a link to an order page on the store for the given product.
+        
+        """
+        url = "https%s/instant%s" % (self.store_url_base, product_path)
+        if referrer is not None:
+            url = "%s?%s" % (url, urllib.urlencode({"referrer": referrer}))
+        return url
---
+++ tests.py
@@ -0,0 +1,211 @@
+# Copyright (C) 2011 by OD Consultancy Ltd.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import unittest
+import urlparse
+from xml.etree import ElementTree
+from mock import Mock
+
+import fastspring
+
+TEST_SUB_XML = """\
+<?xml version="1.0"?>
+<subscription>
+  <status>active</status>
+  <statusChanged>2010-08-15T00:00:00.000Z</statusChanged>
+  <statusReason>completed</statusReason>
+  <cancelable>true</cancelable>
+  <reference/>
+  <test>true</test>
+  <referrer/>
+  <sourceName/>
+  <sourceKey/>
+  <sourceCampaign/>
+  <customer>
+    <firstName/>
+    <lastName/>
+    <company/>
+    <email/>
+    <phoneNumber/>
+  </customer>
+  <customerUrl/>
+  <productName/>
+  <quantity>0</quantity>
+  <nextPeriodDate>2010-08-15Z</nextPeriodDate>
+  <end>2010-08-15Z</end>
+</subscription>"""
+
+TEST_LOCPRI_TEXT = """\
+user_country=US
+user_language=en
+user_currency=USD
+product_1_path=/myproductpath
+product_1_quantity=1
+product_1_unit_value=29.95
+product_1_unit_currency=USD
+product_1_unit_display=$29.95
+product_1_unit_html=$29.95"""
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.fastspring = fastspring.FastSpring("test_company", "test_username", "test_password")
+        self.fastspring.http.request = Mock()
+
+    def mock_response(self, status_code):
+        class Response(object):
+            def __init__(self):
+                self.status = status_code
+        return Response()
+
+    def test_init(self):
+        # make sure internal vars are initialised properly
+        self.assertEqual(self.fastspring.api_url_base, "https://api.fastspring.com/company/test_company")
+        self.assertEqual(self.fastspring.store_url_base, "://sites.fastspring.com/test_company")
+        self.assertEqual(self.fastspring.company, "test_company")
+        self.assertEqual(self.fastspring.username, "test_username")
+        self.assertEqual(self.fastspring.password, "test_password")
+        self.assertEqual(self.fastspring.auth_params, "user=test_username&pass=test_password")
+
+    def test_get_subscription(self):
+        # make sure the correct URL is requested, that data is parsed
+        # correctly and that errors are reported correctly
+        self.fastspring.http.request.return_value = (self.mock_response(200), TEST_SUB_XML)
+        sub = self.fastspring.get_subscription("test_reference")
+        self.assertEqual(sub["status"], "active")
+        self.assertEqual(sub["statusChanged"], "2010-08-15T00:00:00.000Z")
+        self.assertEqual(sub["end"], "2010-08-15Z")
+        self.assertEqual(1, self.fastspring.http.request.call_count)
+        args, kwargs = self.fastspring.http.request.call_args
+        self.assertEqual(kwargs, {})
+        self.assertEqual(len(args), 1)
+        url = urlparse.urlparse(args[0])
+        self.assertEqual(url.scheme, "https")
+        self.assertEqual(url.netloc, "api.fastspring.com")
+        self.assertEqual(url.path, "/company/test_company/subscription/test_reference")
+        qs = urlparse.parse_qs(url.query)
+        self.assertEqual(qs['user'][0], "test_username")
+        self.assertEqual(qs['pass'][0], "test_password")
+        self.fastspring.http.request.return_value = (self.mock_response(404), "Not found")
+        self.assertRaises(fastspring.FastSpringRequestError, self.fastspring.get_subscription, "test_reference")
+        
+    def test_update_subscription(self):
+        # make sure the correct URL is requested and that errors are
+        # reported correctly
+        self.fastspring.http.request.return_value = (self.mock_response(200), "")
+        # test with full arguments
+        self.fastspring.update_subscription("test_reference", product_path="/test_product", quantity=1, no_end_date=True, coupon="test_coupon", proration=True)
+        self.assertEqual(1, self.fastspring.http.request.call_count)
+        args, kwargs = self.fastspring.http.request.call_args
+        self.assertEqual(len(kwargs.keys()), 2)
+        self.assertEqual(kwargs["method"], "PUT")
+        xml = ElementTree.XML(kwargs["body"])
+        self.assertEqual(xml.find("productPath").text, "/test_product")
+        self.assertEqual(xml.find("quantity").text, "1")
+        self.assertEqual(xml.find("no-end-date").text, None)
+        self.assertEqual(xml.find("coupon").text, "test_coupon")
+        self.assertEqual(xml.find("proration").text, "true")
+        self.assertEqual(len(args), 1)
+        url = urlparse.urlparse(args[0])
+        self.assertEqual(url.scheme, "https")
+        self.assertEqual(url.netloc, "api.fastspring.com")
+        self.assertEqual(url.path, "/company/test_company/subscription/test_reference")
+        qs = urlparse.parse_qs(url.query)
+        self.assertEqual(qs['user'][0], "test_username")
+        self.assertEqual(qs['pass'][0], "test_password")
+        # test with partial arguments
+        self.fastspring.update_subscription("test_reference", product_path="/test_product")
+        self.assertEqual(2, self.fastspring.http.request.call_count)
+        args, kwargs = self.fastspring.http.request.call_args
+        self.assertEqual(len(kwargs.keys()), 2)
+        self.assertEqual(kwargs["method"], "PUT")
+        xml = ElementTree.XML(kwargs["body"])
+        self.assertEqual(xml.find("productPath").text, "/test_product")
+        self.assertEqual(xml.find("quantity"), None)
+        self.assertEqual(xml.find("no-end-date"), None)
+        self.assertEqual(xml.find("coupon"), None)
+        self.assertEqual(xml.find("proration"), None)
+        self.assertEqual(len(args), 1)
+        url = urlparse.urlparse(args[0])
+        self.assertEqual(url.scheme, "https")
+        self.assertEqual(url.netloc, "api.fastspring.com")
+        self.assertEqual(url.path, "/company/test_company/subscription/test_reference")
+        qs = urlparse.parse_qs(url.query)
+        self.assertEqual(qs['user'][0], "test_username")
+        self.assertEqual(qs['pass'][0], "test_password")
+        # test error handling
+        self.fastspring.http.request.return_value = (self.mock_response(404), "Not found")
+        self.assertRaises(fastspring.FastSpringRequestError, self.fastspring.update_subscription, "test_reference", product_path="/test_product")
+    
+    def test_delete_subscription(self):
+        # make sure the correct URL is requested, that data is parsed
+        # correctly and that errors are reported correctly
+        self.fastspring.http.request.return_value = (self.mock_response(200), TEST_SUB_XML)
+        sub = self.fastspring.delete_subscription("test_reference")
+        self.assertEqual(sub["status"], "active")
+        self.assertEqual(sub["statusChanged"], "2010-08-15T00:00:00.000Z")
+        self.assertEqual(sub["end"], "2010-08-15Z")
+        self.assertEqual(1, self.fastspring.http.request.call_count)
+        args, kwargs = self.fastspring.http.request.call_args
+        self.assertEqual(len(kwargs.keys()), 1)
+        self.assertEqual(kwargs["method"], "DELETE")
+        self.assertEqual(len(args), 1)
+        url = urlparse.urlparse(args[0])
+        self.assertEqual(url.scheme, "https")
+        self.assertEqual(url.netloc, "api.fastspring.com")
+        self.assertEqual(url.path, "/company/test_company/subscription/test_reference")
+        qs = urlparse.parse_qs(url.query)
+        self.assertEqual(qs['user'][0], "test_username")
+        self.assertEqual(qs['pass'][0], "test_password")
+        # test error handling
+        self.fastspring.http.request.return_value = (self.mock_response(404), "Not found")
+        self.assertRaises(fastspring.FastSpringRequestError, self.fastspring.delete_subscription, "test_reference")
+        
+    
+    def test_get_localised_price(self):
+        # make sure the correct URL is requested, that data is parsed
+        # correctly and errors are reported correctly
+        self.fastspring.http.request.return_value = (self.mock_response(200), TEST_LOCPRI_TEXT)
+        price = self.fastspring.get_localised_price("/test_product", "127.0.0.1", "127.0.0.2", "en")
+        self.assertEqual(price, "$29.95")
+        self.assertEqual(1, self.fastspring.http.request.call_count)
+        args, kwargs = self.fastspring.http.request.call_args
+        self.assertEqual(kwargs, {})
+        self.assertEqual(len(args), 1)
+        url = urlparse.urlparse(args[0])
+        self.assertEqual(url.scheme, "http")
+        self.assertEqual(url.netloc, "sites.fastspring.com")
+        self.assertEqual(url.path, "/test_company/api/price")
+        qs = urlparse.parse_qs(url.query)
+        self.assertEqual(qs['product_1_path'][0], "/test_product")
+        self.assertEqual(qs['user_remote_addr'][0], "127.0.0.1")
+        self.assertEqual(qs['user_x_forwarded_for'][0], "127.0.0.2")
+        self.assertEqual(qs['user_accept_language'][0], "en")
+        # test error handling
+        self.fastspring.http.request.return_value = (self.mock_response(404), "Not found")
+        self.assertRaises(fastspring.FastSpringRequestError, self.fastspring.get_localised_price, "/test_product", "127.0.0.1", "127.0.0.2", "en")
+    
+    def test_short_order_url(self):
+        # make sure the correct URL is returned
+        self.assertEqual(self.fastspring.short_order_url("/test_product"), "https://sites.fastspring.com/test_company/instant/test_product")
+        self.assertEqual(self.fastspring.short_order_url("/test_product", referrer="test_referrer"), "https://sites.fastspring.com/test_company/instant/test_product?referrer=test_referrer")
+
+if __name__ == '__main__':
+    unittest.main()
